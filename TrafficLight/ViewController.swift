e0fae6c1a69fa697d931f66e879f49f5a578cfa6
Vladislav <Belinskii>
2023-04-24 19:04:53 +0300

    Final commit

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -13,15 +13,35 @@ final class ViewController: UIViewController {
     @IBOutlet var greenLight: UIView!
     @IBOutlet var startButton: UIButton!
     
+    let on = 1.0
+    let off = 0.3
+    var tapCounter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLight.layer.cornerRadius = 50
-        yellowLight.layer.cornerRadius = 50
-        greenLight.layer.cornerRadius = 50
-        
+        redLight.layer.cornerRadius = redLight.frame.size.height / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.height / 2
+        greenLight.layer.cornerRadius = greenLight.frame.size.height / 2
     }
 
     @IBAction func startButtonTapped() {
+        
+        tapCounter += 1
+        startButton.setTitle("NEXT", for: .normal)
+        redLight.alpha = off
+        yellowLight.alpha = off
+        greenLight.alpha = off
+        
+        switch tapCounter {
+        case 1:
+            redLight.alpha = on
+        case 2:
+            yellowLight.alpha = on
+        default:
+            greenLight.alpha = on
+            tapCounter = 0
+        }
+        
     }
     
 }
